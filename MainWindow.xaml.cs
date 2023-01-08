52053973e10f5e6733988bea50775c602c46c6fb
maryem ben salem <maryem.bnslm@gmail.com>
2023-01-08 18:15:52 -0500

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace Examen_projet_partie1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<User> users = new List<User>();
+
+        }
+
+      public class User
+        {
+            public int Num { get; set; }
+            public string Nom { get; set; }
+            public string Prenom { get; set; }
+            public string Age { get; set; }
+            public string Sexe { get; set; }
+            public string Programme { get; set; }
+
+
+
+
+        }
+
+        private void DataGrid_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
